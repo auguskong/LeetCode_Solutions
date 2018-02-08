1217ad1c63ef96564acb2feef3d4e170584b7c69
auguskong <xiangyukongdeveloper@gmail.com>
2018-02-07 23:11:18 -0500

    Use two pointers with asynchronous pace to label the start and end of the interval

--- MergeIntervals.java
+++ MergeIntervals.java
@@ -1,7 +1,41 @@
 public List<Interval> merge(List<Interval> intervals) {
+	List<Interval> res = new LinkedList();
+	if (intervals.size() <= 1) {
+		return intervals;
+	}
+
+	int size = intervals.size();
+	int[] starts = new int[size];
+	int[] ends = new int[size];
+	for (int i = 0; i < size; i++) {
+		starts[i] = intervals[i].start; //List use get() function to get value
+		ends[i] = intervals[i].end;
+	}
+
+	//sort is used for simplifying later comparision operation
+	Arrays.sort(starts);
+	Arrays.sort(ends);
+	
+	int i = 0;
+	int j = 0;
+	//use i to track the end, use j to track the start
+	while (i < starts.length) {
+		if (i == starts.length - 1 || ends[i] < starts[i + 1]) {
+				res.add(new Interval(starts[j], ends[i]));
+				j = i + 1; //key point!!
+		}
+		i++; 
+	}
+	return res;
+}
+
+
+//low speed original version
+public List<Interval> merge(List<Interval> intervals) {
+	//corner case check
 	if (intervals.size() <= 1)
 		return intervals;
-
+	//avoid using comparator
 	intervals.sort((i1, i2) -> Integer.compare(i1.start, i2.start));
 
 	//set the start and end to the initial element
