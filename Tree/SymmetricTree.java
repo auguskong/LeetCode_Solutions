57c67da85715daa33567d61c495bcb2b720064cd
auguskong <xkong36@gatech.edu>
2018-03-11 14:19:20 -0400

    recursion need to follow the basic logic

---
+++ Tree/SymmetricTree.java
@@ -0,0 +1,80 @@
+
+/*
+Submission Result: Wrong Answer 
+Input:
+[2,3,3,4,5,5]
+Output:
+true
+Expected:
+false
+*/
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        
+        return helper(root.left, root.right);
+    }
+    
+    public boolean helper(TreeNode root1, TreeNode root2) {
+        if (root1 == null && root2 == null) {
+            return true;
+        }
+        if (root1 == null || root2 == null) {
+            return false;
+        }
+        if (root1.val != root2.val) {
+            return false;
+        }
+        return helper(root1.left, root2.right) || helper(root1.right, root2.left);
+    }
+}
+
+/*
+Submission Result: Wrong Answer 
+Input:
+[2,3,3,4,5,5]
+Output:
+true
+Expected:
+false
+*/
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return helper(root.left, root.right);
+    }
+    
+    public boolean helper(TreeNode root1, TreeNode root2) {
+        if (root1 == null || root2 == null) {
+            return root1 == root2;
+        }
+        if (root1.val != root2.val) {
+            return false;
+        }
+        return helper(root1.left, root2.right) || helper(root1.right, root2.left);
+    }
+}
+
+//Accepted -- Logic error
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return helper(root.left, root.right);
+    }
+    
+    public boolean helper(TreeNode root1, TreeNode root2) {
+        if (root1 == null || root2 == null) {
+            return root1 == root2;
+        }
+        if (root1.val != root2.val) {
+            return false;
+        }
+        return helper(root1.left, root2.right) && helper(root1.right, root2.left);
+    }
+}
