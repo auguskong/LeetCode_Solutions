4ae413eb0deb9c49a358517f446511b8e4c785f2
auguskong <xiangyukongdeveloper@gmail.com>
2018-05-21 23:56:19 -0400

    two variables corresponding to two different exit -> deque

---
+++ StacksAndQueues/SlidingWindowMaximum.java
@@ -0,0 +1,33 @@
+/**
+* Date: 05/21/18
+*	
+*
+*/
+public class Solution {
+	public int[] maxSlidingWindow(int[] a, int k) {
+		if (a == null || k < 1 || a.length < k) {
+			return null;
+		}
+		int n = a.length;
+		int[] res = new int[n - k + 1];
+		int index = 0;
+
+		//The deque store the index of the array
+		Deque<Integer> deque = new ArrayDeque<>();
+		for (int i = 0; i < a.length; i++) {
+			//remove the number that is out of the window range
+			while (!deque.isEmpty() && deque.peek() < i - k + 1) {
+				deque.poll();
+			}
+			//remove smaller numbers inside the window
+			while (!deque.isEmpty() && a[deque.peekLast()] < a[i]) {
+				q.pollLast();
+			}
+			deque.offer(i);
+			if (index >= k - 1) {
+				res[index++] = a[q.peek()];
+			}
+		}
+		return res;
+	}
+}
