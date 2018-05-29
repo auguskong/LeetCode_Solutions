36bdc845ca4dbc546399d4e5e0b45ffa230c3ab9
auguskong <xiangyukongdeveloper@gmail.com>
2018-05-28 23:49:00 -0400

    create new pointer in every loop to finish the skip

--- LinkedList/SwapNodesinPairs.java
+++ LinkedList/SwapNodesinPairs.java
@@ -1,5 +1,17 @@
+/**
+* Date: 05/27/18
+* Des: Given a linked list, swap every two adjacent nodes and return its head.
+* 
+* 1.需要使用,三个指针按照次序依次转换,必须从前往后,否则会导致链表元素的丢失
+* prev.next curr.next next.next 
+* 考虑链表的中间结构是general case, 链表的头部是special case, 头结点没有被一个节点指向，
+* 使用dummy node来构造general case,从而可以使用while loop循环来操作整个链表元素
+* 2. Swap元素是跳跃式的转换，通过`prev = curr;` 来完成，因为curr在转换之后会被换到最前面
+* 
+*/
 
-// 指针调用灵活 保留头结点 + 尾节点
+
+
 class Solution {
     public ListNode swapPairs(ListNode head) {
         if (head == null || head.next == null) {
@@ -9,14 +21,16 @@ class Solution {
         ListNode dummy = new ListNode(0);
         dummy.next = head;
         ListNode prev = dummy;
-        ListNode n1 = head;
-        ListNode n2 = head.next;
         
-        while(n1 != null && n2 != null && n2.next != null) {
-            prev.next = n1.next;
-            n1.next = n2.next;
-            n2.next = n1;
-            prev = n1;
+        while (prev.next != null && prev.next.next != null) {
+            //here create the new pointers and 
+            //start change the pointer direction
+            ListNode curr = prev.next;
+            ListNode next = prev.next.next;
+            prev.next = curr.next;
+            curr.next = next.next;
+            next.next = curr;
+            prev = curr;
         }
         
         return dummy.next;
@@ -25,22 +39,12 @@ class Solution {
 
 class Solution {
     public ListNode swapPairs(ListNode head) {
-        if (head == null || head.next == null) {
+        //出口
+        if ((head == null)||(head.next == null))
             return head;
-        }
-        ListNode dummy = new ListNode(0);
-        dummy.next = head;
-        ListNode prev = dummy;
-    
-        while(prev.next != null && prev.next.next != null) {
-            ListNode n1 = prev.next;
-            ListNode n2 = prev.next.next;
-            prev.next = n2;
-            n1.next = n2.next;
-            n2.next = n1;
-            prev = n1;
-        }
-        
-        return dummy.next;
+        ListNode node = head.next;
+        head.next = swapPairs(head.next.next);
+        n.next = head;
+        return node;
     }
 }
